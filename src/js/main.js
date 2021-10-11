a4a4438789f5a63bd2b7931e8f1333336b9cccbb
retmonhy <retmonhy@gmail.com>
2021-10-11 20:11:12 +0700

    Доверстан интерактивный блок ради которого я решил поучаствовать в интерактиве. Мне это показалось интересным. Оказывается это очень просто реализовывается, есть отбробить рисовку SVG графики для кинозала.

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+const schemeSvg = document.querySelector('.scheme-svg');
+schemeSvg.addEventListener('click', (event) => {
+	let checkBooked = event.target.closest('.qwerty').classList.contains('booked');
+	let checkLegend = event.target.closest('.qwerty').classList.contains('legend');
+	if (!checkBooked && !checkLegend) {
+	event.target.closest('.qwerty').classList.toggle('active');
+	}
+
+	let numSelectedPlaces = document.querySelectorAll('.active').length;
+	let totalValue = calcPrice(numSelectedPlaces);
+	let priceTotalTag = document.querySelector('.price-total', '.price-summ');
+
+	priceTotalTag.textContent = totalValue;
+
+});
+
+function calcPrice(counter) {
+	let pricePerSeat = 400;
+	let totalSum;
+	totalSum = counter * pricePerSeat;
+	return totalSum;
+}
